479d0b993dea0b24880830707e477530790eaa5f
Al Mahmud <mdalmahmud0183@gmail.com>
2018-07-02 07:26:39 +0000

    Assignment1_miniblog

--- controllers/articles.server.controller.js
+++ controllers/articles.server.controller.js
@@ -35,13 +35,12 @@ module.exports.all = function(req, res) {
 
 module.exports.create = function(req, res) {
   var article = new Article(req.body);
-  article.user = req.user;
+  article.postedBy = req.user;
   article.save(function(err, data) {
     if (err) {
       return res.status(400).send({
-
-  				message: errorHandler.getErrorMessage(err)
-  			});
+           message: errorHandler.getErrorMessage(err)
+         });
     } else {
       res.status(200).send(data);
     }
@@ -80,10 +79,31 @@ module.exports.update = function(req, res) {
 };
 
 exports.articleByID = function(req, res, next, id) {
-	Article.findById(id).populate('user', 'email').exec(function(err, article) {
+	Article.findById(id).populate('postedBy', 'username').exec(function(err, article) {
 		if (err) return next(err);
 		if (!article) return next(new Error('Failed to load article ' + id));
 		req.article = article;
 		next();
 	});
 };
+module.exports.single = function(req, res) {
+   res.render('./../public/views/article/view.ejs', {
+          user: req.user || null,
+          article: req.article
+    });
+};
+module.exports.new = function(req, res){
+  res.render('./../public/views/article/new.ejs', {
+          user: req.user || null,
+          request: req
+        });
+};
+module.exports.edit = function(req, res) {
+	res.render('./../public/views/article/edit.ejs', {
+		user: req.user || null,
+		article: req.article
+	});
+};
+
+
+
